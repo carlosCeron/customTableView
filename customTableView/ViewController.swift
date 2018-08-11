5bd9c4c52f9d9914218e0d266b45edf0a0ece0ae
Carlos Ceron Santacruz <carl0scer0n@iMac-de-Carlos.local>
2018-08-11 16:42:42 -0500

    Primera fase del proyecto

---
+++ customTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customTableView
+//
+//  Created by Carlos Ceron Santacruz on 11/08/18.
+//  Copyright © 2018 Carlos Ceron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Custom Tabale View Controller 
